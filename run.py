2eb1f40646ed8092d0ec034067f5341772b47bdb
ebayboy <ebayboy@163.com>
2019-12-24 22:28:55 +0800

    fw

---
+++ run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+#/****************************************************************************
+#@file:run.py 
+#@author:ebayboy@163.com 
+#@date:2019-12-24 22:28 
+#@version 1.0  
+#@description: python file 
+#@Copyright (c)  all right reserved 
+#**************************************************************************/
+
+print("hello")
+
