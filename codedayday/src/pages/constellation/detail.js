d88904e566b8337ddb57937dfbf6cd0767c69edd
ebayboy <ebayboy@163.com>
2019-12-27 22:56:17 +0800

    fw

--- codedayday/src/pages/constellation/detail.js
+++ codedayday/src/pages/constellation/detail.js
@@ -3,22 +3,26 @@ import Taro, { Component } from '@tarojs/taro'
 import './detail.scss'
 
 export default class detail extends Component {
+	state = {
+		constellation: {},
+		allNum : 0, 
+		healthNum: 0, 
+		loveNum: 0,
+		moneyNum: 0,
+		workNum: 0
+	}
+
 	config: Config = {
 		navigationBarTitleText: '详情'
 	}
 
 	componentWillMount () {}
 
-	componentDidMount () {}
-
-	componentDidUnMount () {}
-
-	componentDidShow () {}
-
-	componentDidHide () {}
-
 	render () {
-		return ()
+		return (
+			<View class='page'> 
+			</View>
+		)
 	}
 }
 
